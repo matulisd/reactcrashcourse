66d72444b88723b04ab399310641d85887bee8cd
matulisd <nekton.dominykas@gmail.com>
2022-08-09 01:58:20 +0300

    Basic functionality implementation

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,9 +1,60 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from "react"
+import AddTask from './components/AddTask'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'Movie Night',
+      day: 'Mar 8th at 8:30pm',
+      reminder: false
+    },
+    {
+      id: 3,
+      text: 'School Work',
+      day: 'May 5th at 1:00pm',
+      reminder: true
+    },
+    {
+      id: 4,
+      text: 'Coding Session',
+      day: 'Au 9th at 0:50am',
+      reminder: false
+    }
+  ])
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+    console.log(id)
+  }
+
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
     </div>
   );
 }
